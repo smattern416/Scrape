f6e1fb29453a3a14c9bb86b61f5829a002bed6b2
Stephanie Mattern <smattern416@gmail.com>
2019-07-23 10:49:43 -0400

    installed all required packages

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
