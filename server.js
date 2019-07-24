3636e7c114fddd667f1474417885f988cfcdda6f
Stephanie Mattern <smattern416@gmail.com>
2019-07-24 10:02:13 -0400

    restructured server.js

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+// Dependencies
 var express = require("express");
-var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
 var mongoose = require("mongoose");
-// Require axios and cheerio. This makes the scraping possible
-var axios = require("axios");
+var moment = require("moment");
+
+// Scraping tools
 var cheerio = require("cheerio");
+var request = require("request");
+
+// Require all models
+var db = require("./models");
+
+// Initialize Express
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
