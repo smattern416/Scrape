6c067b149a59a2b832568fb0370fa449fc56ddbf
Stephanie Mattern <smattern416@gmail.com>
2019-07-25 09:27:03 -0400

    restructured server.js and routes

---
+++ controllers/controllers.js
@@ -0,0 +1,7 @@
+
+// Scraping tools
+var cheerio = require("cheerio");
+var request = require("request");
+
+// Require all models
+var db = require("./models");
--- server.js
+++ server.js
@@ -1,21 +1,36 @@
 // Dependencies
 var express = require("express");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
 var mongoose = require("mongoose");
-var moment = require("moment");
-
-// Scraping tools
-var cheerio = require("cheerio");
-var request = require("request");
-
-// Require all models
-var db = require("./models");
+var exphbs = require('express-handlebars');
+var logger = require("morgan");
+// var bodyParser = require("body-parser");
 
 // Initialize Express
 var PORT = process.env.PORT || 3000;
 
 var app = express();
+
+// Require our routes
+var routes = require("./routes/apiRoutes.js");
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect Handlebars to our Express app
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Have every request go through our route middleware
+app.use(routes);
+
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrape_db";
+
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
 
 
 app.listen(PORT, function() {
